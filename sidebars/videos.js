66770eb799471b3a6708f070c2b09bcd2f29c00f
Gaius <gaius.qi@gmail.com>
2022-12-06 11:28:44 +0800

    feat: optimize session videos (#215)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- sidebars/videos.js
+++ sidebars/videos.js
@@ -9,14 +9,30 @@ module.exports = {
           type: 'category',
           label: 'Sessions in English',
           collapsed: false,
-          items: ['sessions/en/2018-12-16','sessions/en/2019-05-22','sessions/en/2019-05-23','sessions/en/2019-07-06','sessions/en/2019-07-07','sessions/en/2019-11-22','sessions/en/2020-12-05','sessions/en/2021-05-15','sessions/en/2022-11-12',],
+          items: [
+            'sessions/en/2022-11-12',
+            'sessions/en/2021-05-15',
+            'sessions/en/2020-12-05',
+            'sessions/en/2019-11-22',
+            'sessions/en/2019-07-06',
+            'sessions/en/2019-05-23',
+            'sessions/en/2019-05-22',
+            'sessions/en/2018-12-16',
+          ],
         },
-       
+
         {
           type: 'category',
           label: 'Sessions in Chinese',
           collapsed: false,
-          items: ['sessions/zh/2020-09-12','sessions/zh/2021-12-19','sessions/zh/2022-07-16','sessions/zh/2022-07-29','sessions/zh/2022-08-09','sessions/zh/2022-08-18',],
+          items: [
+            'sessions/zh/2022-08-18',
+            'sessions/zh/2022-08-09',
+            'sessions/zh/2022-07-29',
+            'sessions/zh/2022-07-16',
+            'sessions/zh/2021-12-19',
+            'sessions/zh/2020-09-12',
+          ],
         },
       ],
     },
